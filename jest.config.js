38ddf037551c526427fa78bc33a660dcc06500c6
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-07-20 17:44:56 +0000

    Release 0.0.1-1

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,4 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    coverageThreshold: {
-      "global": {
-        "branches": 75,
-        "functions": 85,
-        "lines": 90,
-        "statements": 90
-      }
-    }
 };
