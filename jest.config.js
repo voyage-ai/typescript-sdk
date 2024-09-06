75d7e869c24e7f0dca6e4db1e0d9cdf232596c47
fzowl <zoltan@voyageai.com>
2024-09-06 15:35:38 +0200

    Adding back the coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,12 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
+    coverageThreshold: {
+      "global": {
+        "branches": 95,
+        "functions": 95,
+        "lines": 95,
+        "statements": 95,
+      }
+    }
 };
